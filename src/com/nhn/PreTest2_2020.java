a81ef9913774219a5f9f8921937508b3f158e084
ohsj <zasqw456@namver.com>
2020-10-24 21:55:13 +0900

    preTest2 can't solved

---
+++ src/com/nhn/PreTest2_2020.java
@@ -0,0 +1,68 @@
+package com.nhn;
+
+public class PreTest2_2020 {
+    //      3
+//      3 % % 3
+//      3 % % 3
+//      3 % 3 3
+//      3 3 3 3 % 2
+//3 % % 3 3 3 3 % 2
+//3 % % 3 3 3 3 % 2 3
+//3 3 % 3 3 3 3 % 2 3
+//3 3 % 3 3 2 * * 2 3
+//3 3 % 3 3 2 * * 2 2
+//3 2 * * * 2 * * 2 2
+//2 1 * * * 2 * * 2 2
+//2 1 2 * * 2 * * 2 2
+//2 1 2 * 2 2 * * 2 2
+//2 1 2 * 2 2 * * 2 2
+//2 1 # # 1 2 * * 2 2
+//2 1 # # 1 # # 1 2 2
+//1 1 # # 1 # # 1 # 1
+//1 1 # # 1 # # 1 # 1
+//1 1 # # 1 1 # 1 # 1
+//1 1 # # 1 1 # 1 1 1
+//1 1 # 1 1 1 # 1 1 1
+//1 1 # 1 1 1 # 1 1 1
+//------------------------------
+//1 27
+//2 26
+//3 16
+//-------
+//  69
+    private static void solution(int day, int width, int[][] blocks) {
+        int answer = 0;
+        int[] wall = new int[width];
+
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < width; j++) {
+                wall[j] += blocks[i][j];
+            }
+
+            int before = 0;
+            for (int j = 1; j < wall.length - 1; j++) {
+                int a = wall[j-1];
+                int b = wall[j+1];
+                int min = Math.min(a,b);
+
+                if (wall[j] < min) {
+                    answer+= min;
+                    wall[j] = min;
+                    if (before != 0 && min > before) {
+                        answer+=min;
+                    }
+                    before = min;
+                }
+            }
+
+        }
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) {
+        // 20
+//        solution(2, 6, new int[][]{{6, 2, 11, 0, 3, 5}, {6, 3, 0, 9, 0, 5}});
+        // 69
+        solution(3, 10, new int[][]{{6, 12, 0, 2, 8, 4, 0, 7, 3, 6}, {6, 1, 3, 0, 2, 8, 0, 0, 13, 8}, {6, 3, 0, 10, 6, 5, 7, 0, 0, 3}});
+    }
+}
